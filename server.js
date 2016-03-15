2040629ee6a5e97373e3bd30b15664112acfb763
Rob Ostheimer <robostheimer@yahoo.com>
2016-03-14 22:48:25 -0700

    added CORS

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
+app.use(allowCrossDomain); //allow for CORS requests
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
