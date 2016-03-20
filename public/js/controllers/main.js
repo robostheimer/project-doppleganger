07f68335e52d320429eede91092a9e1dea6be7ea
Rob Ostheimer <robostheimer@yahoo.com>
2016-03-19 19:26:24 -0700

    changed timeout debouncing

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -68,40 +68,6 @@ angular.module('Ganger', [])
 	}
 }])
 
-.directive('ngDelay', ['$timeout', function ($timeout) {
-    return {
-        restrict: 'A',
-        scope: true,
-        compile: function (element, attributes) {
-            var expression = attributes['ngChange'];
-            if (!expression)
-                return;
-
-            var ngModel = attributes['ngModel'];
-            if (ngModel) attributes['ngModel'] = '$parent.' + ngModel;
-            attributes['ngChange'] = '$$delay.execute()';
-
-            return {
-                post: function (scope, element, attributes) {
-                    scope.$$delay = {
-                        expression: expression,
-                        delay: scope.$eval(attributes['ngDelay']),
-                        execute: function () {
-                            var state = scope.$$delay;
-                            state.then = Date.now();
-                            $timeout(function () {
-                            	console.log(state.then)
-                                if (Date.now() - state.then >= state.delay)
-                                    scope.$parent.$eval(expression);
-                            }, state.delay);
-                        }
-                    };
-                }
-            }
-        }
-    };
-}])
-
 .controller('getYelpResults', ['$scope','$http','SearchYelp', function($scope, $http, SearchYelp) {	 
 
 	$scope.places = true;
