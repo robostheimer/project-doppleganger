1ae9d87b5df12d6849a3f1123dba8a42295af695
Rob Ostheimer <robostheimer@yahoo.com>
2016-03-20 19:47:11 -0700

    added geolocation

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -68,14 +68,61 @@ angular.module('Ganger', [])
 	}
 }])
 
-.controller('getYelpResults', ['$scope','$http','SearchYelp', function($scope, $http, SearchYelp) {	 
+.factory('Geolocation', ['$http', '$q', 'CityFind',
+function($http, $q, CityFind) {
+	return {
 
+		checkGeolocation: function() {
+			var deferred = $q.defer();
+			navigator.geolocation.getCurrentPosition(function(data){
+					CityFind.findCity(data.coords.latitude, data.coords.longitude, .01).then(function(location){
+						console.log(location)
+					deferred.resolve({lat: data.coords.latitude, lon: data.coords.longitude, city:location.city, state: location.state});
+				})	
+			}, function(error){
+				deferred.resolve(false);
+			});
+			return deferred.promise;
+		}
+	};
+}])
+
+.factory('CityFind', ['$http', 
+	function($http) {
+	return {
+			findCity : function(lat, lng, ratio) {
+				var url = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+CityName%2C+Region%2C+CountryID+FROM+1B8NpmfiAc414JhWeVZcSqiz4coLc_OeIh7umUDGs+WHERE+Lat+<=" + (lat+ratio) + "+AND+Lat>=" + (lat - ratio) + "+AND+Long<=" + (lng+ratio) + "+AND+Long>=" + (lng -ratio) + "&key=AIzaSyBBcCEirvYGEa2QoGas7w2uaWQweDF2pi0";
+				return $http.get(url).then(function(data) {
+					if (data.data.rows != null) {
+						return {
+							city: data.data.rows[0][0], 
+							state: data.data.rows[0][1]
+						};					
+					}
+				});
+			}
+		};
+	}])
+
+.controller('getYelpResults', ['$scope','$http','SearchYelp', 'Geolocation', '$q',
+	function($scope, $http, SearchYelp, Geolocation, $q) {	 
 	$scope.places = true;
 	$scope.keywords = false;
-	$scope.placesButtonState='off';
-	$scope.keywordsButtonState='on';
-
-
+	$scope.placesButtonState ='off';
+	$scope.keywordsButtonState ='on';
+	$scope.addTypeahead = true;
+	$scope.loading = true;
+	Geolocation.checkGeolocation().then(function(data){
+		if(data!==false)
+		{
+			$scope.geolocation = data.lat +', '+data.lon;
+			$scope.whereyouare = data.city+', '+data.state;
+			$scope.addTypeahead=true;
+			$scope.loading=false
+		}
+		$scope.addTypeahead = false;
+		$scope.loading=false
+	});
 	$scope.runSearch = function(searchterms, location) {
 		SearchYelp.searchYelp(searchterms, location).then(function(result){
 		 		$scope.keyword_results =result.businesses; 
@@ -96,20 +143,21 @@ angular.module('Ganger', [])
 		 			});
 		 		}); 
 		 });	
-	}
+	};
+
 	$scope.showKeywords = function() {
 		$scope.places = true;
 		$scope.keywords = false;
 		$scope.placesButtonState='off';
 		$scope.keywordsButtonState='on';
 
-	}
+	};
 
 	$scope.showPlaces = function() {
 		$scope.places = false;
 		$scope.keywords = true;
 		$scope.placesButtonState='on';
 		$scope.keywordsButtonState='off';
-	}
+	};
 
 }]);		
