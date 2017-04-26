c893dc65d0c2d952f55dd1a3a8b51043b89ea014
Rob Ostheimer <robostheimer@yahoo.com>
2017-04-25 22:59:32 -0700

    changed maps

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -8,12 +8,12 @@ angular.module('Ganger', [])
 
 				if($rootScope.geolocation) {
 				return $http.get('/api/yelp/search?term='+searchterms+'&location='+location+'&cll='+$rootScope.geolocation.together+'&limit=10').then(function(result) {
-					
+
 					return result.data;
 				});
 			} else {
 					return $http.get('/api/yelp/search/?term='+searchterms+'&location='+location+'&limit=10').then(function(result) {
-						
+
 						return result.data;
 				});
 			}
@@ -21,7 +21,7 @@ angular.module('Ganger', [])
 
 			searchYelpBusiness: function(id) {
 				return $http.get('/api/yelp/business/?name='+id).then(function(result) {
-					
+
 					return result.data;
 			});
 		}
@@ -36,28 +36,28 @@ function($http, $q, CityFind) {
 			navigator.geolocation.getCurrentPosition(function(data){
 					CityFind.findCity(data.coords.latitude, data.coords.longitude, .05).then(function(location){
 						deferred.resolve({lat: data.coords.latitude, lon: data.coords.longitude, city:location.city, state: location.state});
-				})	
+				})
 			}, function(error){
 				deferred.resolve(false);
 			});
-			
+
 			return deferred.promise;
 		},
 	};
 }])
 
-.factory('CityFind', ['$http', 
+.factory('CityFind', ['$http',
 	function($http) {
 	return {
 			findCity : function(lat, lng, ratio) {
 				var url = "https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+CityName%2C+Region%2C+CountryID+FROM+1B8NpmfiAc414JhWeVZcSqiz4coLc_OeIh7umUDGs+WHERE+Lat+<=" + (lat+ratio) + "+AND+Lat>=" + (lat - ratio) + "+AND+Long<=" + (lng+ratio) + "+AND+Long>=" + (lng -ratio) + "&key=AIzaSyBBcCEirvYGEa2QoGas7w2uaWQweDF2pi0";
-				
+
 				return $http.get(url).then(function(data) {
 					if (data.data.rows != null) {
 						return {
-							city: data.data.rows[0][0], 
+							city: data.data.rows[0][0],
 							state: data.data.rows[0][1]
-						};					
+						};
 					}
 				});
 			}
@@ -65,7 +65,7 @@ function($http, $q, CityFind) {
 	}])
 
 .controller('getYelpResults', ['$scope','$rootScope','$http','SearchYelp', 'Geolocation', '$q',
-	function($scope, $rootScope, $http, SearchYelp, Geolocation, GeolocationFind, $q) {	 
+	function($scope, $rootScope, $http, SearchYelp, Geolocation, GeolocationFind, $q) {
 	$scope.places = true;
 	$scope.keywords = false;
 	$scope.placesButtonState ='off';
@@ -122,19 +122,19 @@ function($http, $q, CityFind) {
 	});
 
 	$scope.runSearch = function(searchterms, location) {
-		
+
 		$scope.keyword_results = [];
 		$rootScope.markers =[];
 		$scope.noBusinesses=false;
 
-		
+
 		if((searchterms !== undefined && searchterms !== "") && (location !== undefined && location !== ""))
 		{
 			$scope.searching = true;
 			$scope.loading = true;
 
 			SearchYelp.searchYelp(searchterms, location).then(function(result) {
-				
+
 
 				if(result.businesses.length === 0)
 	 			{
@@ -142,19 +142,19 @@ function($http, $q, CityFind) {
 	 				$scope.searching = false;
 	 			} else {
 	 				//$rootScope.geolocation .together = ""
-	 				
+
 	 				if($rootScope.geolocation.together === "" || $rootScope.geolocation.together === undefined || $rootScope.geolocation.city_state!==location) {
 						$rootScope.geolocation.together = result.businesses[0].location.coordinate.latitude +', '+result.businesses[0].location.coordinate.longitude;
 						$rootScope.geolocation.lat = result.businesses[0].location.coordinate.latitude;
 						$rootScope.geolocation.lon = result.businesses[0].location.coordinate.longitude;
 						$rootScope.geolocation.orig_lat = result.businesses[0].location.coordinate.latitude;
 						$rootScope.geolocation.orig_lon = result.businesses[0].location.coordinate.longitude;
-					}	
+					}
 
-			 	$scope.keyword_results =result.businesses; 
+			 	$scope.keyword_results =result.businesses;
 
 			 	result.businesses.forEach(function(item) {
-			 		item.showIndivMap = false;	
+			 		item.showIndivMap = false;
 			 		item.timesShowed = 0;
  					$rootScope.markers.push(item);
  				});
@@ -166,13 +166,13 @@ function($http, $q, CityFind) {
 			});
 		}
 	}
-	
+
 	$scope.runBusinessSearch = function(searchterms, location, destination) {
-		
+
 		$scope.placesyoulike_results = [];
 		$rootScope.markers =[];
 		$scope.noBusinesses=false;
-		
+
 		if((searchterms !== undefined && searchterms !== "") && (location !== undefined && location !== "")  && (destination !== undefined && destination !== ""))
 		{
 			$scope.searching = true;
@@ -187,7 +187,7 @@ function($http, $q, CityFind) {
 	 			}
 	 			else {
 			 		var id =result.businesses[0].id
-			 		SearchYelp.searchYelpBusiness(id).then(function(data) {		
+			 		SearchYelp.searchYelpBusiness(id).then(function(data) {
 				 			var categoriesStr = '',
 				 			placesCategoriesStr = '';
 				 			data.categories.forEach(function(item) {
@@ -195,7 +195,7 @@ function($http, $q, CityFind) {
 				 			});
 
 				 			SearchYelp.searchYelp(categoriesStr, destination).then(function(moredata) {
-				 				
+
 								if(result.businesses.length == 0)
 					 			{
 					 				$scope.noBusinesses=true;
@@ -210,17 +210,17 @@ function($http, $q, CityFind) {
 										$rootScope.geolocation.orig_lat = moredata.businesses[0].location.coordinate.latitude;
 										$rootScope.geolocation.orig_lon = moredata.businesses[0].location.coordinate.longitude;
 									}
-					 				
+
 					 				//$scope.placesyoulike_results = moredata.businesses;
 					 				$scope.enableButtons($scope.viewIcons);
-					 				
+
 					 				moredata.businesses.forEach(function(item) {
 					 					var o = moredata.businesses.indexOf(item)
 					 					data.categories.forEach(function(second_item){
 					 						if(item.categories[0][0] === second_item[0]) {
 					 							if(!placesCategoriesStr.match(second_item[0]))
 					 							{
-					 								placesCategoriesStr += item.categories[0][0]; 
+					 								placesCategoriesStr += item.categories[0][0];
 					 							}
 					 						}
 					 					});
@@ -230,15 +230,15 @@ function($http, $q, CityFind) {
 				 						$rootScope.markers.push(item);
 				 					});
 				 					$scope.markers_holder = $rootScope.markers;//creates a cache of the markers
-					 			}	
+					 			}
 					 			//add another check to rank results - basically re run SearchYelp.searchYelp for city of place you like to with the top three results to see if place you like is actually a doppleganger
-					 			
+
 								$scope.reOrderBusinesses(moredata.businesses, location, destination, placesCategoriesStr, result.businesses[0].name);
-						 		
-				 			});		
+
+				 			});
 				 		});
-			 		} 
-			 });	
+			 		}
+			 });
 		}
 	};
 
@@ -260,7 +260,7 @@ function($http, $q, CityFind) {
 			 			SearchYelp.searchYelp(categoriesStr, destination).then(function(moredata) {
 			 				var i;
 			 				lngth+=moredata.businesses.length
-			 				
+
 			 				for(i=0; i<moredata.businesses.length; i++) {
 			 					j++
 			 					if(moredata.businesses[i].name === orig_searchterms)
@@ -270,8 +270,8 @@ function($http, $q, CityFind) {
 				 					result.businesses[0].showIndivMap = false;
 				 					result.businesses[0].timesShowed = 0;
 				 					arr.splice(0, 0, result.businesses[0]);
-				 					
-				 					
+
+
 			 					}
 			 					if(j==lngth)
 				 					{
@@ -281,11 +281,11 @@ function($http, $q, CityFind) {
 					 					$scope.loading = false;
 			 						}
 			 				}
-			 			});		
+			 			});
 			 		});
-			 	})	
+			 	})
 			})
-			//remove Duplicates and keep lower ranked 
+			//remove Duplicates and keep lower ranked
 			//problem with sorting algorithm in prototypes.js
 		}
 	};
@@ -296,9 +296,9 @@ function($http, $q, CityFind) {
 	custom markers ?
 	Add # of miles and ability to get directions using ArcGIS
 	Color Code the cards by ranking
-**/	
+**/
 	$scope.showMap = function() {
-		$scope.noMap = false;		
+		$scope.noMap = false;
 		$rootScope.markers = $scope.markers_holder;
 	}
 
@@ -335,7 +335,7 @@ function($http, $q, CityFind) {
 		$scope.noMap = true;
 		$rootScope.markers = $scope.markers_holder;
 	}
-	
+
 	$scope.showKeywords = function() {
 		$scope.places = true;
 		$scope.keywords = false;
@@ -349,20 +349,20 @@ function($http, $q, CityFind) {
 		$scope.placesButtonState='on';
 		$scope.keywordsButtonState='off';
 	};
-	
+
 	$scope.enableButtons = function(btnArr) {
 		btnArr.forEach(function(item){
 			item.status = 'enabled'
 		});
 	}
-	
+
 	$scope.changeView = function(type) {
 		$scope.viewIcons.forEach(function(item, name){
 			if(item.status !== 'disabled') {
 
 				if(type==item.name)
 				{
-					item.state = 'on';	
+					item.state = 'on';
 					item.class = 'show';
 				} else {
 					item.state = 'off';
@@ -376,46 +376,44 @@ $scope.createIndivMap = function(item, index, type) {
 	var mapId = type+index,
 
 	map = new L.Map(mapId,{}),
-	
-	INDIV_MAP = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-				subdomains: '1234',
-				mapID: 'newest',
-				app_id: 'Y8m9dK2brESDPGJPdrvs',
-				app_code: 'dq2MYIvjAotR8tHvY8Q_Dg',
-				base: 'base',
-				minZoom: 0,
-				maxZoom: 20					
-				}),
-	
+
+	INDIV_MAP = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+		subdomains: 'abcd',
+		minZoom: 0,
+		maxZoom: 20,
+		ext: 'png'
+	});
+
 	circle = L.circle([$rootScope.geolocation.orig_lat, $rootScope.geolocation.orig_lon], 125, {
 				    color: '#428bca',
 				    fillColor: '#428bca',
 				    fillOpacity: 0.15
 					})
-	marker_content = '<a href="'+item.url+'" target="_blank">'+item.name+'</a><br>'+item.location.address[0]+'<br>'+item.location.city+'<br><a href="tel://'+item.display_phone+'">'+item.display_phone+'</a><br><img src="'+item.rating_img_url+'" alt="'+item.rating+' stars">', 
-	
+	marker_content = '<a href="'+item.url+'" target="_blank">'+item.name+'</a><br>'+item.location.address[0]+'<br>'+item.location.city+'<br><a href="tel://'+item.display_phone+'">'+item.display_phone+'</a><br><img src="'+item.rating_img_url+'" alt="'+item.rating+' stars">',
+
 	marker = L.marker([item.location.coordinate.latitude, item.location.coordinate.longitude]).bindPopup(marker_content);
-	
+
 	map.animate=true;
 	map._zoom = 13	;
 	map.scrollWheelZoom.disable();
 	map.panTo([item.location.coordinate.latitude, item.location.coordinate.longitude]);
  	map.zoomControl.options.position='topright';
 	if($scope.geolocationUsed){
-		circle.addTo(map);	
+		circle.addTo(map);
 	}
 	marker.addTo(map);
 	map.addLayer(INDIV_MAP);
-	$('#'+mapId).css({'width': '100%'});	
+	$('#'+mapId).css({'width': '100%'});
 }
 
 }])
 
 .directive('dgMap',  function ($rootScope) {
-	
+
  return {
 	 restrict: 'AE',
-     scope: { }, 
+     scope: { },
      transclude: true,
 
     link: function(scope, element, attr ) {
@@ -427,7 +425,7 @@ $scope.createIndivMap = function(item, index, type) {
 			$rootScope.geolocation.orig_lon=0;
 			$rootScope.geolocation.zoom=13;
 			$rootScope.mapOpening=true;
-	        	
+
 
 			var map = new L.Map("map",{});
 			var markers=[];
@@ -439,7 +437,7 @@ $scope.createIndivMap = function(item, index, type) {
 			//var Esri_WorldGrayCanvas = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
 			//var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 			//var Esri_WorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-	//});			
+	//});
 			//var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', { attribution: 'Tiles &copy; Esri&mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
 
 			var MAP = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
@@ -449,12 +447,12 @@ $scope.createIndivMap = function(item, index, type) {
 				app_code: 'dq2MYIvjAotR8tHvY8Q_Dg',
 				base: 'base',
 				minZoom: 0,
-				maxZoom: 20					
+				maxZoom: 20
 				});
 			var marker_content = '';
 
-			map.addLayer(MAP); 	
-    	   	  	
+			map.addLayer(MAP);
+
     	attr.$observe('change', function() {
 
     		markers.forEach(function(item){
@@ -462,9 +460,9 @@ $scope.createIndivMap = function(item, index, type) {
     		});
 				if($rootScope.geolocation.orig_lat === undefined || $rootScope.geolocation.orig_lon === undefined || $rootScope.geolocation.orig_lat===0 || $rootScope.geolocation.orig_lon === 0)
 					{
-						$rootScope.geolocation.orig_lat = $rootScope.geolocation.lat 
+						$rootScope.geolocation.orig_lat = $rootScope.geolocation.lat
 						$rootScope.geolocation.orig_lon = $rootScope.geolocation.lon
-					}	
+					}
 				var circle = L.circle([$rootScope.geolocation.orig_lat, $rootScope.geolocation.orig_lon], 125, {
 				    color: '#428bca',
 				    fillColor: '#428bca',
@@ -490,7 +488,7 @@ $scope.createIndivMap = function(item, index, type) {
 				markers.forEach(function(marker) {
 					marker.addTo(map);
 				});
-      }); 
-    }  
+      });
+    }
    };
-	});		     
+	});
